511dd4dcbc64942d93561cd1ea17b2e49df81528
DarthByte <neverovm28@gmail.com>
2025-05-08 20:55:02 +0400

    feat: Add user urls

--- todolist/todolist/urls.py
+++ todolist/todolist/urls.py
@@ -4,4 +4,5 @@ from django.urls import include, path
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("tasks/", include("tasks.urls", namespace="tasks")),
+    path("users/", include("users.urls", namespace="users")),
 ]
