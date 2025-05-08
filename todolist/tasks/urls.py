b3d3ada8c1d7e7d77452dff1b0faf501f096bffd
DarthByte <neverovm28@gmail.com>
2025-05-08 19:03:02 +0400

    feat: Add new url patterns for views

--- todolist/tasks/urls.py
+++ todolist/tasks/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 
-from tasks.views import TaskListCreateAPIView
+from tasks.views import (
+    TaskListCreateAPIView,
+    TaskRetrieveUpdateDestroyAPIView,
+    CategoryListCreateAPIView,
+    CategoryRetrieveUpdateDestroyAPIView,
+)
 
 app_name = "task"
 
@@ -8,7 +13,21 @@ urlpatterns = [
     path(
         "api/v1/task/",
         TaskListCreateAPIView.as_view(),
-        name="task-list-destroy-create",
+        name="task-create",
     ),
-
+    path(
+        "api/v1/task/<str:task_id>/",
+        TaskRetrieveUpdateDestroyAPIView.as_view(),
+        name="task-get-update-delete",
+    ),
+    path(
+        "api/v1/category/",
+        CategoryListCreateAPIView.as_view(),
+        name="category-create",
+    ),
+    path(
+        "api/v1/category/<str:category_id>/",
+        CategoryRetrieveUpdateDestroyAPIView.as_view(),
+        name="category-get-update-delete",
+    ),
 ]
