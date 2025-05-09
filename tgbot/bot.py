95c4996bfb146d64554e2f5c440c1ebd4e23be5d
DarthByte <neverovm28@gmail.com>
2025-05-09 12:38:55 +0400

    fix: Add method for creation category

--- tgbot/bot.py
+++ tgbot/bot.py
@@ -8,7 +8,9 @@ from aiogram_dialog import setup_dialogs
 from tgbot.config import TOKEN_BOT
 from tgbot.handlers.start import router as cmd_router
 from tgbot.task.handlers import router as task_router
+from tgbot.category.handlers import router as category_router
 from tgbot.task.dialog import get_dialog_task
+from tgbot.category.dialog import get_dialog_category
 
 
 
@@ -21,7 +23,9 @@ async def main():
     setup_dialogs(dp)
     dp.include_router(cmd_router)
     dp.include_router(task_router)
+    dp.include_router(category_router)
     dp.include_router(get_dialog_task())
+    dp.include_router(get_dialog_category())
 
     await dp.start_polling(bot)
 
