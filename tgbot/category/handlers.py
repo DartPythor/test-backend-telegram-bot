2b2cda1f4fdddcc84f45ac38187b9c331dbfb4bf
DarthByte <neverovm28@gmail.com>
2025-05-09 15:25:39 +0400

    feat: Add function to delete categories

--- tgbot/category/handlers.py
+++ tgbot/category/handlers.py
@@ -3,14 +3,25 @@ from aiogram import F, Router
 from aiogram.types import CallbackQuery, Message
 from aiogram_dialog import DialogManager, StartMode
 
-from tgbot.category.state import CategoryState
+from tgbot.category.state import CategoryState, CategoryListState, CategoryDeleteState
 from tgbot.keyboard.task_menu import category_menu
 
 router = Router()
 
+
 @router.callback_query(F.data == "category-create")
 async def start_task_create(callback: CallbackQuery, dialog_manager: DialogManager):
     await dialog_manager.start(CategoryState.name, mode=StartMode.RESET_STACK)
+
+
+@router.callback_query(F.data == "category-list")
+async def start_task_create(callback: CallbackQuery, dialog_manager: DialogManager):
+    await dialog_manager.start(CategoryListState.list, mode=StartMode.RESET_STACK)
+
+
+@router.callback_query(F.data == "category-delete")
+async def start_task_create(callback: CallbackQuery, dialog_manager: DialogManager):
+    await dialog_manager.start(CategoryDeleteState.category_id, mode=StartMode.RESET_STACK)
 
 
 @router.message(F.text == "Теги")
