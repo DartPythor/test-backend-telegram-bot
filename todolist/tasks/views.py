3e982c757382805336ed82e09e5ce506f0ea7f72
DarthByte <neverovm28@gmail.com>
2025-05-09 11:58:43 +0400

    fix: Add user field in serializer

--- todolist/tasks/views.py
+++ todolist/tasks/views.py
@@ -10,21 +10,6 @@ class TaskListCreateAPIView(generics.ListCreateAPIView):
     serializer_class = TaskSerializer
     lookup_field = "task_id"
 
-    def get_queryset(self):
-        telegram_id = self.request.query_params.get("telegram_id", None)
-        if telegram_id is None:
-            raise ValidationError({"telegram_id": "This field is required."})
-        try:
-            user = CustomUser.objects.get(telegram_id=telegram_id)
-        except CustomUser.DoesNotExist:
-            raise ValidationError({"telegram_id": "Invalid telegram_id."})
-
-        return Tasks.objects.filter(user=user)
-
-    def perform_create(self, serializer):
-        user = CustomUser.objects.get(telegram_id=self.request.data["telegram_id"])
-        serializer.save(user=user)
-
 
 class TaskRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Tasks.objects.all()
@@ -35,21 +20,7 @@ class TaskRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
 class CategoryListCreateAPIView(generics.ListCreateAPIView):
     queryset = Categories.objects.all()
     serializer_class = CategorySerializer
-
-    def get_queryset(self):
-        telegram_id = self.request.query_params.get("telegram_id", None)
-        if telegram_id is None:
-            raise ValidationError({"telegram_id": "This field is required."})
-        try:
-            user = CustomUser.objects.get(telegram_id=telegram_id)
-        except CustomUser.DoesNotExist:
-            raise ValidationError({"telegram_id": "Invalid telegram_id."})
-
-        return Categories.objects.filter(user=user)
-
-    def perform_create(self, serializer):
-        user = CustomUser.objects.get(telegram_id=self.request.data["telegram_id"])
-        serializer.save(user=user)
+    lookup_field = "category_id"
 
 
 class CategoryRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
